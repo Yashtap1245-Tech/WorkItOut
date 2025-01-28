2eaa6d32e2bc5bb01b8ae03a803198014a2e5492
Yashtap1245-Tech <yashtap1245@gmail.com>
2025-01-28 12:10:05 -0800

    workout plan class added

---
+++ lib/model/workout_plan.dart
@@ -0,0 +1,12 @@
+import 'package:workout_tracker/model/exercise.dart';
+
+class WorkoutPlan {
+  final String name;
+
+  final List<Exercise> exercises;
+
+  const WorkoutPlan({
+    required this.name,
+    required this.exercises,
+  });
+}
